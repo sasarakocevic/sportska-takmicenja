66d9d5bacf671cadb4d6d9a31e6250b8314b25c0
Dusan Matkovic <matkovic.dusan99@gmail.com>
2022-06-04 11:55:56 +0200

    Added Controller and Routes for Tim

---
+++ back-end/app/Http/Controllers/TimController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tim;
+
+class TimController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Tim::all(); //Model get all
+        return $data;
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = Tim::create($request->all());
+        return $data;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Tim::findOrFail($id);
+        return $data;
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $tim = Time::find($id);
+        $tim->update($request->all()); //model update
+        return $tim;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tim = Tim::findOrFail($id);
+        $tim->delete($id);
+        return'{"success":"Uspjesno ste obrisali tim."}';
+    }
+}
